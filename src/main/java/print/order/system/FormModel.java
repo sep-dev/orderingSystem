bec51d33f19ebd67386b372d7bd1298b2e4a3ca6
福島 佑也 <fukushimayuya1102@ad.se-project.co.jp>
2016-07-07 14:47:58 +0900

    詳細画面

--- src/main/java/print/order/system/FormModel.java
+++ src/main/java/print/order/system/FormModel.java
@@ -1,5 +1,13 @@
 package print.order.system;
 
 public class FormModel {
+	private String up;
 
+	public String getUp() {
+		return up;
+	}
+
+	public void setUp(String up) {
+		this.up = up;
+	}
 }
--- src/main/java/print/order/system/HomeController.java
+++ src/main/java/print/order/system/HomeController.java
@@ -1,7 +1,6 @@
 package print.order.system;
 
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 
 import javax.servlet.http.HttpSession;
@@ -13,46 +12,27 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.support.SessionStatus;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
-
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 
-	@RequestMapping(value = "/list", method = RequestMethod.GET)
-	public String login(Locale locale, Model model) {
-		List<Map<String, Object>> list = jdbcTemplate
-				.queryForList("select * from ordertb inner join clienttb on ordertb.orderid = clienttb.orderid;");
-		model.addAttribute("db", list);
-		model.addAttribute("id", list.get(0).get("id"));
-		return "list";
-	}
+	@RequestMapping(params = "inquiry", method = RequestMethod.POST)
+	public String aa(@ModelAttribute FormModel fm, Model model, HttpSession session) {
+		String quiry = fm.getUp();
 
-	@RequestMapping(value = "/menu", method = RequestMethod.GET)
-	public String logi(Locale locale, Model model) {
+		List<Map<String, Object>> lista = jdbcTemplate.queryForList(
+				"select * from ordertb inner join clienttb on ordertb.orderid = clienttb.orderid where ordertb.orderid = "
+						+ "'" + quiry + "'");
+		model.addAttribute("data", lista);
+		session.setAttribute("Id", quiry);
+		model.addAttribute("id", quiry);
 
-		return "menu";
-	}
-
-	@RequestMapping(value = "/Frame", method = RequestMethod.GET)
-	public String log(Locale locale, Model model) {
-
-		return "Frame";
-	}
-
-	@RequestMapping(params = "list", method = RequestMethod.POST)
-	public String bb(@ModelAttribute FormModel fm, Model model, SessionStatus sessionStatus, HttpSession session) {
-		List<Map<String, Object>> list = jdbcTemplate
-				.queryForList("select * from ordertb inner join clienttb on ordertb.orderid = clienttb.orderid;");
-		model.addAttribute("db", list);
-		model.addAttribute("id", list);
-
-		return "list";
+		return "inquiry";
 	}
 
 }
